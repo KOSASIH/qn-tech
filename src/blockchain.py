85014048e83c827113b8fee6139b743b2756d4a4
KOSASIH <kosasihg88@gmail.com>
2025-01-05 19:56:42 +0700

    Create blockchain.py

---
+++ src/blockchain.py
@@ -0,0 +1,14 @@
+# src/blockchain.py
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def add_block(self, block):
+        # Implement logic to add a block to the chain
+        pass
+
+    def validate_chain(self):
+        # Implement logic to validate the blockchain
+        pass
