76c61e22798b338ef11dc911c0c878cc592368e7
KOSASIH <kosasihg88@gmail.com>
2025-01-05 10:08:51 +0700

    Create GovernanceView.java

---
+++ src/main/java/com/qntech/governance/GovernanceView.java
@@ -0,0 +1,15 @@
+package com.qntech.governance;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GovernanceView {
+
+    public void displayProposalForm() {
+        // Logic to render a form for creating a proposal
+    }
+
+    public void displayVotingResults(ProposalResults results) {
+        // Logic to render the results of a proposal vote
+    }
+}
